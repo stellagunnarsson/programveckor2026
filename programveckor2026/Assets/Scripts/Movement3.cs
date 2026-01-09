53fa6e3d9a7879a2c619fc35cc1e07d8ed3af413
stellagunnarsson <stella.gunnarsson@elev.ga.lbs.se>
2026-01-09 23:23:25 +0100

    dialog?

---
+++ programveckor2026/Assets/Scripts/Movement3.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Movement3 : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+
+            rb.linearVelocity = new Vector2(5, 0);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+
+            rb.linearVelocity = new Vector2(-5, 0);
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+
+            rb.linearVelocity = new Vector2(0, 5);
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+
+            rb.linearVelocity = new Vector2(0, -5);
+
+        }
+
+    }
+}
---
+++ programveckor2026/Assets/Scripts/NPC.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    public GameObject dialougePanel;
+    public TextMeshProUGUI dialougeText;
+    public string[] dialouge;
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && playerIsClose)
+        {
+            if (dialougePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialougePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+
+        if (dialougeText.text == dialouge[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    public void zeroText()
+    {
+        dialougeText.text = "";
+        index = 0;
+        dialougePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialouge[index].ToCharArray())
+        {
+            dialougeText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+
+        contButton.SetActive(false);
+
+        if (index < dialouge.Length - 1)
+        {
+            index++;
+            dialougeText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
