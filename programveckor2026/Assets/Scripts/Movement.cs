f45609bddeffe7c23e6fe68023662a7033632339
stellagunnarsson <stella.gunnarsson@elev.ga.lbs.se>
2026-01-12 15:53:44 +0100

    .

---
+++ programveckor2026/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+
+            rb.linearVelocity = new Vector2(7, 0);
+
+
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+
+
+            rb.linearVelocity = new Vector2(-7, 0);
+
+
+
+        }
+
+    }
+}
