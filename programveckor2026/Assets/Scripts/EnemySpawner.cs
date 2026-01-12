51280b3e5561d407510da787b54ae7472d4e8eef
★ Sally ★ <sally.axberg@elev.ga.lbs.se>
2026-01-12 09:50:39 +0100

    bunni bunni
    la till bunni och försökte fixa så man kan dö typ

---
+++ programveckor2026/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode fireButton;
+
+    [SerializeField]
+    Vector2 fireDirection = Vector2.zero;
+
+    [SerializeField]
+    float coolDown;
+
+    [SerializeField]
+    GameObject prefab;
+    [SerializeField]
+    Transform spawnLocation;
+
+
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer -= Time.deltaTime;
+
+
+
+        if ((fireButton == KeyCode.None || Input.GetKeyDown(fireButton)) && timer <= 0)
+        {
+
+            Vector2 pos = transform.position;
+            if (spawnLocation != null)
+            {
+                pos = spawnLocation.position;
+            }
+
+            GameObject spawnedObject = Instantiate(prefab, pos, Quaternion.identity);
+
+            Rigidbody2D rb = spawnedObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.linearVelocity = fireDirection;
+            }
+
+
+            timer = coolDown;
+        }
+
+
+
+
+    }
+}
