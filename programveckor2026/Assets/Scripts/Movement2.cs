d5d3fb0ee7b73e4e8c5c8b6b364c79dfcec5a079
stellagunnarsson <stella.gunnarsson@elev.ga.lbs.se>
2026-01-20 14:07:36 +0100

    Update Movement2.cs

--- programveckor2026/Assets/Scripts/Movement2.cs
+++ programveckor2026/Assets/Scripts/Movement2.cs
@@ -4,7 +4,9 @@ public class Movement2 : MonoBehaviour
 {
 
     Rigidbody2D rb;
-
+    public GameObject dialoguePanel;
+    public GameObject dialoguePanel2;
+    public GameObject dialoguePanel3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,28 +18,33 @@ public class Movement2 : MonoBehaviour
     void Update()
     {
 
-
-
-        rb.linearVelocity = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        if (!dialoguePanel.activeInHierarchy || !dialoguePanel2.activeInHierarchy || !dialoguePanel3.activeInHierarchy)
         {
-            if (transform.position.x < 8.27)
-            {
-                rb.linearVelocity = new Vector2(5, 0);
-            }
-
+            rb.linearVelocity = new Vector2(0, 0);
 
         }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        else
         {
-            if (transform.position.x > -8.27)
+            rb.linearVelocity = new Vector2(0, 0);
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             {
-                rb.linearVelocity = new Vector2(-5, 0);
+                if (transform.position.x < 8.27)
+                {
+                    rb.linearVelocity = new Vector2(5, 0);
+                }
+
+
             }
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                if (transform.position.x > -8.27)
+                {
+                    rb.linearVelocity = new Vector2(-5, 0);
+                }
 
 
+            }
         }
-
     }
 }
